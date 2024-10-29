16df36ab5139313f3c6e133783a46bac816826b4
boxlerr <boxlerjulian@hotmail.com>
2024-10-29 15:06:18 -0300

    JSX FRAGMENT

--- src/index.js
+++ src/index.js
@@ -4,8 +4,28 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Greeting() {
-  const married = false;
-  return <h1>{married ? "Estoy casado 😶‍🌫️" : "No estoy casado 😮‍💨"}</h1>;
+
+  function add(x, y) {
+    return x + y;
+  }
+  const user = {
+    firstName: "Julian",
+    lastName: "Boxler",
+  };
+  return (
+    <>
+      <h1>{user.firstName}</h1>
+      <h3>{user.lastName}</h3>
+      <h3>{add(20, 20)}</h3>
+    </>
+  );
 }
 
-root.render(<Greeting />);
+root.render( //FRAGMENT, si eliminamos la etiqueta nos evitamos el div dentro de otro div, funciona sin tener nada ahora gracias a JSX
+  <> 
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+  </>
+);
