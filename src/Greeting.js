585b104a5bd17be56eebfe63ac2065bd5f6f937c
boxlerr <boxlerjulian@hotmail.com>
2024-10-30 16:25:03 -0300

    Props

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,10 +1,23 @@
-export function Greeting() {
-    function add(x, y) {
-      return x + y;
-    }
-    return <h1>{add(20, 20)}</h1>;
-  }
+export function Greeting({ title, name = "user" }) {
+  return (
+    <h1>
+      {title} dice, {name}
+    </h1>
+  );
+}
 
-export function UserCard(){
-    return <h1>User Card</h1>
-  }
+export function UserCard(props) {
+  //puedo estructurarlo sin props ({name,amount,etc})
+  console.log(props);
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <p>💲{props.amount}</p>
+      <p>{props.married ? "Married" : "Single"}</p>
+      <ul>
+        <li>City: {props.address.city}</li>
+        <li>Adress: {props.address.street}</li>
+      </ul>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {Greeting, UserCard } from './Greeting'
-import Product,{Navbar} from './Product'
+import { Greeting, UserCard } from "./Greeting";
+import Product, { Navbar } from "./Product";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-
 
 root.render(
   <>
-    <Greeting />
-    <UserCard/>
-    <Product/>
-    <Navbar/>
+    <UserCard
+      name="Ryan Ray"
+      amount={3000}
+      married={true}
+      points={[99, 33.3, 22.2]}
+      address={{ street: "123 Main Street", city: "New York" }}
+      greet={function () {alert('Hello')}} // puedo enviar funciones tambien por js
+    />
+
+    <UserCard
+    name="Matias Suarez"
+    amout={1000}
+    married={false}
+    points={[22,45]}
+    address={{
+        street:"Av Sarmiento",
+        city:"New York"
+    }}
+    />
   </>
 );
