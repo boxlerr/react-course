0d8ec86725e5ce1d1b7f76704f8b9d14711a11bb
boxlerr <boxlerjulian@hotmail.com>
2024-10-30 12:37:19 -0300

    Modulos Exportar e Importar js

---
+++ src/Greeting.js
@@ -0,0 +1,10 @@
+export function Greeting() {
+    function add(x, y) {
+      return x + y;
+    }
+    return <h1>{add(20, 20)}</h1>;
+  }
+
+export function UserCard(){
+    return <h1>User Card</h1>
+  }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import {Greeting, UserCard } from './Greeting'
+import Product,{Navbar} from './Product'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Greeting() {
 
-  function add(x, y) {
-    return x + y;
-  }
-  const user = {
-    firstName: "Julian",
-    lastName: "Boxler",
-  };
-  return (
-    <>
-      <h1>{user.firstName}</h1>
-      <h3>{user.lastName}</h3>
-      <h3>{add(20, 20)}</h3>
-    </>
-  );
-}
 
-root.render( //FRAGMENT, si eliminamos la etiqueta nos evitamos el div dentro de otro div, funciona sin tener nada ahora gracias a JSX
-  <> 
+root.render(
+  <>
     <Greeting />
-    <Greeting />
-    <Greeting />
-    <Greeting />
+    <UserCard/>
+    <Product/>
+    <Navbar/>
   </>
 );
